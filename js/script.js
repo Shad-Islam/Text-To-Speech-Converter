275fafbbca3eefb4ffc4127ba3825e9b7e44dd94
Shad-islam <shadislam48@gmail.com>
2023-10-09 01:51:11 +0600

    Add voice change option

--- js/script.js
+++ js/script.js
@@ -1,7 +1,21 @@
+let voices = [];
 let speech = new SpeechSynthesisUtterance();
 const input = document.querySelector("#input-text");
 const button = document.querySelector("#listen-btn");
 const SelectVoice = document.querySelector("#select-voice");
+
+window.speechSynthesis.onvoiceschanged = () => {
+  voices = window.speechSynthesis.getVoices();
+  speech.voice = voices[0];
+
+  voices.forEach(
+    (voice, i) => (SelectVoice.options[i] = new Option(voice.name, i))
+  );
+};
+
+SelectVoice.addEventListener("change", () => {
+  speech.voice = voices[SelectVoice.value];
+});
 
 button.addEventListener("click", () => {
   speech.text = input.value;
