61116b5291e04c2f9989c9a2caf8b7a8c14170dd
Shad-islam <shadislam48@gmail.com>
2023-10-09 00:47:21 +0600

    Change identifier

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 let speech = new SpeechSynthesisUtterance();
-const button = document.querySelector("button");
-const input = document.querySelector("textarea");
+const input = document.querySelector("#input-text");
+const button = document.querySelector("#listen-btn");
+const SelectVoice = document.querySelector("#select-voice");
 
 button.addEventListener("click", () => {
   speech.text = input.value;
